8e3a912b41288499b0a0210558faa28bba2a80c8
Abhishek Kalyane <63503591+abhishek-designs@users.noreply.github.com>
2020-11-13 11:26:26 +0530

    Now songs can also be played directly from the songs list

--- dist/scripts/navBarControl.js
+++ dist/scripts/navBarControl.js
@@ -1,6 +1,7 @@
 // Selecting the navbar's component
 const navbar = document.querySelector('#navbar-m');
 const toggler = navbar.querySelector('.toggler');
+const songsContain = document.querySelector('#main-section');
 const navLinks = navbar.querySelector('.nav-links');
 
 toggler.addEventListener('click',(e) => {
@@ -14,11 +15,13 @@ toggler.addEventListener('click',(e) => {
 const openNav = () => {
     navbar.style.width = 'auto';
     navLinks.style.opacity = 1;
+    songsContain.style.marginLeft = '0.6rem';
 }
 
 const closeNav = () => {
     navbar.style.width = 0;
     navLinks.style.opacity = 0;
+    songsContain.style.marginLeft = 0;
 }
 
 // console.log(checked);
